b9303494a2c3e6f4f3788a2d2d2b8b945a13c5d6
aron-dan <82916233+aron-dan@users.noreply.github.com>
2021-10-16 20:40:04 -0300

    Add project files.

---
+++ zero0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zero0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaraão de variaveis
+            
+            float result, n1, n2;
+
+            //entrada
+            Console.WriteLine("Digite um valor:");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor:");
+            n2 = float.Parse(Console.ReadLine());
+
+            //processamento
+            result = n1 / n2;
+
+            while (n1 == 0 || n2 == 0)
+            {
+                Console.WriteLine("Impossivel dividir por zero, tente de novo");
+
+                Console.WriteLine("Digite um valor:");
+                n1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite um valor:");
+                n2 = float.Parse(Console.ReadLine());
+            }
+
+
+            //saida
+            Console.WriteLine("o valor é:" + result.ToString("F5"));
+            Console.ReadKey();
+        }
+    }
+}
